184d94339527cd81efac0411acc77c6d72ae6605
pradeep meena <137433025+pradeepmeena05@users.noreply.github.com>
2023-09-11 19:23:06 +0530

    code added

---
+++ variable/variable.js
@@ -0,0 +1,12 @@
+//Always use const if the type should not be changed (Arrays and Objects)
+ //Only use let if you can't use const.
+//Only use var if you MUST support old browsers.
+
+let name ="pradeep";
+let city ="bhopal"
+console.log(name)
+console.log(city)
+
+const gender ='male';
+
+console.log(gender);
