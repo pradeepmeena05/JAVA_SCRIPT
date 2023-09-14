dc0bb09d3b699c5b17448d5dfd5c4df8b2e945a8
pradeep meena <137433025+pradeepmeena05@users.noreply.github.com>
2023-09-14 12:10:12 +0530

    code added
    #string

---
+++ Switch/try.js
@@ -0,0 +1,15 @@
+let grade="A";
+
+switch(grade)
+{
+    case "A":
+        console.log("Grade A very good");
+        break;
+
+   case "B":
+        console.log("keep learning");
+        break;
+    default:
+        console.log("invalid grade");    
+
+}
