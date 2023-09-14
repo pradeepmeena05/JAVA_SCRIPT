bb1df085333e3cbf96f5d9b11cae09cd40f9ffec
pradeep meena <137433025+pradeepmeena05@users.noreply.github.com>
2023-09-14 12:11:02 +0530

    code added
    # type_conversion

---
+++ Type_conversion/num_to_boolean.js
@@ -0,0 +1,10 @@
+//javaScript considers 0 as false and all non zero numbers as true
+
+
+
+let a=8;
+
+a=Boolean(a);
+
+console.log('value of a is',a);
+console.log('type of a is',typeof(a));
---
+++ Type_conversion/num_to_string.js
@@ -0,0 +1,6 @@
+let num=80;
+
+num=String(num);
+
+console.log('value of num');
+console.log('type of num',typeof(num));
