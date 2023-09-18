69f4ea39808055f570599037903656fff891d457
pradeep meena <137433025+pradeepmeena05@users.noreply.github.com>
2023-09-18 18:13:31 +0530

    code added

---
+++ console/script.js
@@ -0,0 +1,18 @@
+
+console.log("This is console");
+
+console.error("hey! this is error");
+
+console.assert("this is assert");
+
+console.warn("this is warn");
+
+console.info("this is info");
+
+obj={firstname:"pradeep",lastname:"meena"}
+
+
+console.table(obj);
+
+let arr=Array.from(document.body.childNodes);
+console.log(arr)
