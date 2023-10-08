8873b77f04c96404aacdbaf22915c7e243dd9521
pradeepmeena05 <pradeepmeena34330@gmail.com>
2023-10-08 13:22:11 +0500

    code added

---
+++ practice4/script.js
@@ -0,0 +1,14 @@
+
+let x=function(e)
+{
+    let a=prompt("enter one number")
+    let b=prompt("enter second number")
+    
+    let sum=a*b;
+    console.log(sum)
+  
+}
+
+
+btn1.addEventListener('click',x)
+
