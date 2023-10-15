342fc2f9e38801f3d134d1c22a630af2181b60a5
pradeepmeena05 <pradeepmeena34330@gmail.com>
2023-10-15 11:08:39 +0500

    code added

---
+++ Error_Handling/script.js
@@ -0,0 +1,25 @@
+ 
+    setTimeout(()=>{
+    console.log("hello1")
+},1000)
+
+setTimeout(()=>{
+    try
+    {
+         console.log(pr)
+    }
+    catch(err)
+{
+    console.log("error found")
+}
+},2000)
+
+setTimeout(()=>{
+    console.log("hello3")
+},3000)
+
+setTimeout(()=>{
+    console.log("hello4")
+},4000)
+
+
